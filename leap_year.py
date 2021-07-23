164fef534144e04d474b73fb207ee08680c99279
GeorgesBOUM <boumgeorges@protonmail.com>
2021-07-23 15:24:30 +0200

    snippet to check a leap year

---
+++ leap_year.py
@@ -0,0 +1,8 @@
+year = int(input("choose the year to check if leap: "))
+if (year % 4 != 0):
+    print(year, " is not a leap year")
+elif (year % 100 == 0):
+    if (year % 400 == 0):
+        print(year, " is a leap year")
+    else:
+        print(year, " is not a leap year")
