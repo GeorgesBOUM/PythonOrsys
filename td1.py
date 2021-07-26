10447bdc411c27f6c8d5b4e36a01f4ebd857f2be
GeorgesBOUM <boumgeorges@protonmail.com>
2021-07-26 09:31:47 +0200

    exercice 1

---
+++ td1.py
@@ -0,0 +1,9 @@
+'''
+Exercice 1: Ecrire un programme en langage Python qui demande à l’utilisateur
+de saisir son nom et de lui afficher son nom avec un message de bienvenue !
+'''
+def read_and_print_name():
+    name = input("type your name: ")
+    print("the name you typed is " + name)
+
+
