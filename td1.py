e3a41ea553caf79cc9d51d2eee8df993b0e43106
GeorgesBOUM <boumgeorges@protonmail.com>
2021-07-26 10:35:19 +0200

    exercice 2

--- td1.py
+++ td1.py
@@ -6,4 +6,13 @@ def read_and_print_name():
     name = input("type your name: ")
     print("the name you typed is " + name)
 
-
+'''
+Exercice 2: Ecrire un programme en Python (une fonction) qui demande
+à l’utilisateur de saisir deux nombres a et b et de lui afficher leur somme :
+a + b (passage par argument)
+'''
+def sum_two_numbers():
+    first_number = float(input("type the first number: "))
+    second_number = float(input("type the second number: "))
+    print(f"the sum of {first_number} and {second_number}\
+    is {first_number + second_number}")
